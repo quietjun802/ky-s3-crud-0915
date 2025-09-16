cf33201471e4480c66de218e572b29fbf3e2eda0
quietjun802 <whdydwns802@gmail.com>
2025-09-16 21:16:21 +0900

    1

--- backend/routers/files.js
+++ backend/routers/files.js
@@ -5,6 +5,7 @@ import {
     presignGet,
     presignPut,
     deleteObject} from '../src/s3.js'
+import FileItem from "../models/FIleItem.js";
 
 const router = Router()
 
@@ -28,6 +29,109 @@ router.post('/presign',async(req,res)=>{
     }
 })
 
+router.post('/',async(req,res)=>{
+    try {
+
+        const {
+            key,
+            originalName,
+            contentType,
+            size,
+            title="",
+            description=""
+        }=req.body
+
+        const doc = await FileItem.create({
+            key,
+            originalName,
+            contentType,
+            size,
+            title,
+            description
+        })
+        res.status(201).json({message:"S3 메타데이터 저장 완료",doc})
+        
+    } catch (error) {
+        console.error('메타데이터 저장 에러,error')
+        res.status(500).json({error:"S3 메타데이터 저장 실패",doc})
+        
+    }
+})
+
+router.get('/',async(req,res)=>{
+    try {
+
+        const items = (await FileItem.find()).toSorted({createdAt:-1}).lean()
+
+        const out = await Promise.all(
+            items.map(async(it)=>({
+                ...it,
+                url:await presignGet(it.key,300)
+            }))
+        )
+
+        res.status(201).json({message:"S3 메타데이터 저장 완료",doc})
+    } catch (error) {
+        console.error('메타데이터 저장 에러,error')
+        res.status(500).json({error:"S3 메타데이터 저장 실패",doc})
+    }
+})
+
+router.get('/:id',async(req,res)=>{
+    try {
+
+        const it = await FileItem.findById(req.params.id).lean()
+
+        if(!it) return res.sendStatus(404)
+        it.url = await presignGet(it.key,300)
+        
+
+        res.status(201).json({message:"S3 메타데이터 단권 가져오기",it})
+    } catch (error) {
+        console.error('메타데이터 저장 에러,error')
+        res.status(500).json({error:"S3 메타데이터 저장 실패"})
+    }
+})
+
+router.patch('/:id',async(req,res)=>{
+    try {
+        const {title, description}=req.body
+        const it = await FileItem.findByIdAndUpdate(
+            req.params.id,
+            {title, description},
+            {new:true}
+        )
+
+        if(!it) return res.sendStatus(404)
+        
+            it.url = await presignGet(it.key,300)
+        
+
+        res.status(201).json({message:"S3 메타데이터 수정하기성공",it})
+    } catch (error) {
+        console.error('메타데이터 저장 에러,error')
+        res.status(500).json({error:"S3 메타데이터 저장 실패"})
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+
+
+        const it = await FileItem.findByIdAndDelete(req.params.id)
+
+        if (!it) return res.sendStatus(404)
+
+            await deleteObject(it.key)
+            await it.deleteOne()
+
+        res.status(201).json({ message: "파일 삭제 완료",id: req.params.id })
+
+    } catch (error) {
+        console.error('파일 삭제 에러', error)
+        res.status(500).json({ error: "S3 파일 삭제 실패" })
+    }
+})
 
 
 export default router
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import FileList from './components/FileList'
+import UploadForm from './components/UploadForm'
+function App() {
+ 
+
+  return (
+    <div className='container'>
+    <UploadForm/>
+      <FileList/>
+      </div>
+  )
+}
+
+export default App
